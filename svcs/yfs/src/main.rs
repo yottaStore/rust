933a7060e62a54c2e48ac67c2cc0b9c1717a457e
espoal <alberto.websci@gmail.com>
2022-11-18 15:03:41 +0100

    feat(yfs): io_uring open

--- svcs/yfs/src/main.rs
+++ svcs/yfs/src/main.rs
@@ -1,4 +1,5 @@
 use io_uring::{opcode, types, IoUring};
+use std::ffi::CString;
 use std::os::unix::io::AsRawFd;
 use std::{fs, io};
 
@@ -6,8 +7,28 @@ fn main() -> io::Result<()> {
     let mut ring = IoUring::new(8)?;
 
     let path = "/home/mamluk/yotta/rust/README.md";
-    let fd = fs::File::open(path)?;
+    //let fd = fs::File::open(path)?;
     let mut buf = vec![0; 1024];
+
+    let dirfd = types::Fd(libc::AT_FDCWD);
+    let openhow = types::OpenHow::new().flags(libc::O_DIRECT as _);
+    let path = CString::new(path.as_bytes())?;
+
+    let open_e = opcode::OpenAt2::new(dirfd, path.as_ptr(), &openhow)
+        .build()
+        .user_data(0x41);
+
+    unsafe {
+        ring.submission()
+            .push(&open_e)
+            .expect("submission queue is full");
+    }
+
+    ring.submit_and_wait(1)?;
+
+    let cqe = ring.completion().next().expect("completion queue is empty");
+
+    let fd = cqe.result();
 
     let read_e = opcode::Read::new(types::Fd(fd.as_raw_fd()), buf.as_mut_ptr(), buf.len() as _)
         .build()
